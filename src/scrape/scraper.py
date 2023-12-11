d6fdea08f792fec2a525126255f2e6ef73f2497e
soroush <s.ghaderi@itoll.ir>
2023-12-11 11:12:17 +0330

    [FIX] refactore scraper

--- src/scrape/scraper.py
+++ src/scrape/scraper.py
@@ -49,18 +49,17 @@ def main():
 
     list_of_game_ids = get_daily_game_ids(games_date=arguments.date)
     logging.info("all game ids of specified date is available now.")
-    if not os.path.isfile(os.path.join(file_path["tracking_scrape"], f"scrape_status_{arguments.date.replace('-', '')}.csv")):
+    if not os.path.isfile(os.path.join(file_path["tracking_scrape"], f"{arguments.date.replace('-', '')}.csv")):
         scraper_dataframe = build_date_possible_informations(list_of_game_ids=list_of_game_ids, scrape_date=arguments.date)
         logging.info("dataframe created of all possible scrapes for games and parameters.")
-        scraper_dataframe.to_csv(os.path.join(file_path["tracking_scrape"], f"scrape_status_{arguments.date.replace('-', '')}.csv"), index=False,)
+        scraper_dataframe.to_csv(os.path.join(file_path["tracking_scrape"], f"{arguments.date.replace('-', '')}.csv"), index=False,)
     else:
-        scraper_dataframe = pd.read_csv(os.path.join(file_path["tracking_scrape"],f"scrape_status_{arguments.date.replace('-', '')}.csv"))
+        scraper_dataframe = pd.read_csv(os.path.join(file_path["tracking_scrape"],f"{arguments.date.replace('-', '')}.csv"))
 
     for row_num, value in scraper_dataframe.iterrows():
         valid_links_per_game_id = build_stats_links(game_id=value["game_id"])
         logging.info(f"all links {value['game_id']} are created for scraping.")
 
-        # loop through parameters
         for parameter, scraper in dict_of_scrapers.items():
             if value[parameter] == "-" and parameter in dict_of_active_scrapes.keys():
                 try:
@@ -72,16 +71,16 @@ def main():
                     logging.info("value at scrape dataframe corrected.")
 
                     scraper_dataframe.to_csv(
-                        os.path.join(file_path["tracking_scrape"], f"scrape_status_{arguments.date.replace('-', '')}.csv",), index=False)
+                        os.path.join(file_path["tracking_scrape"], f"{arguments.date.replace('-', '')}.csv",), index=False)
                 except:
                     scraper_dataframe.loc[row_num, parameter] = 0
-
             else:
                 logging.info(f"{parameter} not valid to scrape.")
             time.sleep(2)
 
 
 if __name__ == "__main__":
+    
     logging.basicConfig(
         filename="tennis_loggs.log",
         filemode="a",
